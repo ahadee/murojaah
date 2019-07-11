f075a4157ac163e3531c6d26213a3edccb3423f6
hanif27th <hanif03@gmail.com>
2019-07-12 03:22:01 +0700

    add sidebar on app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import './App.css';
+import { Menu, Icon, Container, Sidebar, Segment } from "semantic-ui-react";
 import MainApp from './mainApp';
 
 class App extends React.Component {
-  render() {  
+  state ={
+    sideVisibility: false
+  }
+  handleSidebarShow = () => {
+    console.log("clicked");
+    
+    this.setState({ sideVisibility: true })
+  }
+
+  handleSidebarHide = () => this.setState({ sideVisibility: false })
+  render() { 
+    const sideVisibility = this.state.sideVisibility; 
     return (
-      <MainApp />
+      <div>
+      <Sidebar.Pushable>
+              
+        <Sidebar 
+          as={Menu} 
+          animation='overlay' 
+          inverted 
+          vertical 
+          visible={sideVisibility}           
+          onHide={() => this.handleSidebarHide()}               
+          width='wide'
+        >
+        <Menu.Item onClick={this.handleSidebarHide}>
+          <Icon name="sidebar" /> Menu
+        </Menu.Item>
+        <Menu.Item>Juz 30 Utuh</Menu.Item>
+        <Menu.Item>Juz 30 Utuh</Menu.Item>
+        <Menu.Item>Juz 30 Utuh</Menu.Item>
+        <Menu.Item>Juz 30 Utuh</Menu.Item>
+        </Sidebar>
+        <Sidebar.Pusher dimmed={sideVisibility} >
+        <Menu attached='top'>
+          <Container>
+            <Menu.Item as='a' onClick={this.handleSidebarShow}>
+              <Icon name="sidebar" /> Menu
+            </Menu.Item>
+          </Container>
+        </Menu>
+        <MainApp></MainApp>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>    
+      
+      </div>
     );
   }
 }
